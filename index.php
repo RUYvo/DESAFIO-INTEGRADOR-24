96b615c9bd5541561f67c07dfcfb2eeaa59041a9
Gustavo Silva <gustavosilvam85@gmail.com>
2024-05-28 11:57:48 -0300

    Frontend 80% visual

--- index.php
+++ index.php
@@ -20,78 +20,172 @@
 <body>
     <div class="container m-6">
         <!-- logo -->
-   <div class="row">
-       <div class="col-8"> 
-            <div>
-                <p class="h2 p-3 ">Clima tempo</p>
-            </div>
+        <div class="row ">
+            <div class="col-lg-8"> 
+                <div>
+                    <p class="h2 p-3 ">Clima tempo</p>
+                </div>
             
-       </div>
-       <!-- barra de pesquisa -->
-       <div class="col-4 py-4"> 
-            <input class="form-control " type="text" placeholder="Digite sua cidade">
-       </div>
-   </div>
+            </div>
+            <!-- barra de pesquisa -->
+            <div class="col-lg-4 py-4"> 
+                <input class="form-control " type="text" placeholder="Digite sua cidade">
+            </div>
+        </div>
    <!-- card principal -->
-   <div class="row d-flex">
-        <div class="card py-4"> 
-            <div class="col-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path id="a" d="M49.5 25C35.5 25 24 36.4 24 50.5 24 64.5 35.4 76 49.5 76 63.5 76 75 64.6 75 50.5 75 36.5 63.6 25 49.5 25z"/></defs><g fill="none" fill-rule="evenodd"><use fill="#FFAD00" fill-opacity=".1" xlink:href="#a"/><path stroke="#FFAC00" stroke-width="4" d="M49.5 27C36.5 27 26 37.5 26 50.5S36.5 74 49.5 74 73 63.5 73 50.5 62.5 27 49.5 27z"/><path stroke="#FFAC00" stroke-width="4" d="M50.5 18V9M82 50.5h9M9 51h9M20 80l7-7M72 28l7-7M28 28l-8-8M80 80l-8-8M50.5 91v-9" stroke-linecap="round"/></g></svg>
+    <div class="card">
+        <div class="card-body">
+            <div class="row ">
+                <div class="col-lg-5 text-center">
+                    <img class="img-fluid w-50" src=".//src/climas/clear_day.svg">
+                </div>
+
+                <div class="col-lg-5 align-self-center">
+                    <p class="h2 text-center">Guarapuava - Paraná - Brasil</p>
+
+                     <div class="row ">
+                        <div class="col-lg h1 text-center  align-self-center">
+                            <p>26º</p>
+                        </div>
+                        <div class="col-lg text-center  align-self-center">
+                            <p>Ensolarado <p>
+                         </div>
+                    <div class="col-lg">
+                        <div class="row text-end">
+                            <p>Amanhacer:</p>
+                        </div>
+                        <div class="row text-end">
+                            <p>Entardecer:</p> 
+                        </div>
+                    </div>
+                    <div class="col-lg h4">
+                        <p>19:00</p>
+                        <div class="row">
+                            <p>19:00</p>
+                            </div>
+                    </div>
+                
+                    </div>
+                </div>
             </div>
-            <div class="col-3">
-                <p class="h2">Guarapuava - Paraná - Brasil</p>
-            </div>
-            
-   </div>
-           
+        </div>
+    </div>
+        
    <!-- cards -->
-   <div class="row d-flex justify-content-between mt-3">
-        <div class="col-2">
+   <div class="row d-flex justify-content-between mt-5">
+        <div class="col-lg-2">
             <div class="card">
-                <div class=" text-center">
+                <div class="card-body text-center">
                     <h5 class="card-title">Segunda</h5>
-                    <p class="card-text">XDASDASDAS</p>
+                    <img class="img-fluid" src="./src/climas/rain.svg">
+                    <p class="card-text">Chuvoso</p>
                     <p class="card-text">26º</p>
                 </div>
             </div>
         </div>
-        <div class="col-2">
+        <div class="col-lg-2">
             <div class="card" >
                 <div class="card-body text-center">
                     <h5 class="card-title">Terça</h5>
-                    <p class="card-text">XDASDASDAS</p>
+                    <img class="img-fluid" src="./src/climas/rain.svg">
+                    <p class="card-text">Chuvoso</p>
                     <p class="card-text">26º</p>
                 </div>
             </div>
         </div>
-        <div class="col-2">
+        <div class="col-lg-2">
             <div class="card" >
                 <div class="card-body text-center">
                     <h5 class="card-title">Quarta</h5>
-                    <p class="card-text">XDASDASDAS</p>
+                    <img class="img-fluid" src="./src/climas/rain.svg">
+                    <p class="card-text">Chuvoso</p>
                     <p class="card-text">26º</p>
                 </div>
             </div>
         </div>
-        <div class="col-2">
+        <div class="col-lg-2">
             <div class="card" >
                 <div class="card-body text-center">
                     <h5 class="card-title">Quinta</h5>
-                    <p class="card-text">XDASDASDAS</p>
+                    <img class="img-fluid" src="./src/climas/rain.svg">
+                    <p class="card-text">Chuvoso</p>
                     <p class="card-text">26º</p>
                 </div>
             </div>
         </div>
-        <div class="col-2">
+        <div class="col-lg-2">
             <div class="card">
                 <div class="card-body text-center">
                     <h5 class="card-title">Sexta</h5>
-                    <p class="card-text">XDASDASDAS</p>
+                    <img class="img-fluid" src="./src/climas/rain.svg">
+                    <p class="card-text">Chuvoso</p>
                     <p class="card-text">26º</p>
                 </div>
             </div>
         </div>
-       </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col">
+            <div class="card p-3">
+                <div class="card-body d-flex ">
+                    <div class="col">
+                        <p class="text-center">Probabilidade de Chuva</p>
+                        <div class="row">
+                            <div class="col text-end h2">
+                                <p>16</p>
+                            </div>
+                            <div class="col align-self-center ">
+                                <p>%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <p class="text-center">Volume de chuva (ultima hora)</p>
+                        <div class="row">
+                            <div class="col text-end h2">
+                                <p>2</p>
+                            </div>
+                            <div class="col align-self-center">
+                                <p>mm</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card p-3">
+                <div class="card-body d-flex ">
+                    <div class="col">
+                        <p class="text-center">Umidade</p>
+                        <div class="row">
+                            <div class="col text-end h2">
+                                <p>5</p>
+                            </div>
+                            <div class="col align-self-center">
+                                <p>%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <p class="text-center">Nebulosidade</p>
+                        <div class="row align-self-center">
+                            <div class="col text-end h2">
+                                <p>16</p>
+                            </div>
+                            <div class="col align-self-center">
+                                <p>%</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+  
+</div>
    </div>
 </body>
 </html>
